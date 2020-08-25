752f82550051b707c21ea4d92d674b50ce078ec6
hidefumiyasui <skyblue6000@icloud.com>
2020-08-25 17:31:31 +0900

    画像投稿機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,9 +5,21 @@ class ItemsController < ApplicationController
     @items = Item.all.order(id: 'DESC')
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
   private
 
   def move_to_index
     redirect_to action: :index unless user_signed_in?
+  end
+
+  def item_params
+    params.require(:item).permit(:image_url, :name, :explanation, :categorie_id, :condition_id, :shipping_charge_id, :shipping_region_id, :delivery_days_id, :price,).merge(user_id: current_user.id)
   end
 end

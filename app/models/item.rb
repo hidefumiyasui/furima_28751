ab8cbc802b58f4234b0cc5a230fa1401cc5fb3f3
hidefumiyasui <skyblue6000@icloud.com>
2020-08-27 17:22:33 +0900

    非同期的に販売手数料及び販売利益の変化の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,14 +8,15 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_region
   belongs_to_active_hash :delivery_days
 
-  validates :categorie_id, numericality: { other_than: 1 } 
-  validates :condition_id, numericality: { other_than: 1 } 
-  validates :shipping_charge_id, numericality: { other_than: 1 } 
-  validates :shipping_region_id, numericality: { other_than: 1 } 
-  validates :delivery_days_id, numericality: { other_than: 1 }
   validates :image_url, presence: true
   validates :name, presence: true
   validates :explanation, presence: true
-  validates :price, presence: true,
-            length: { minimum: 300, maximum: 9999999999 }
+  validates :categorie_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :shipping_charge_id, numericality: { other_than: 1 }
+  validates :shipping_region_id, numericality: { other_than: 1 }
+  validates :delivery_days_id, numericality: { other_than: 1 }
+  validates :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 300 }  
+  validates :price, numericality: { less_than_or_equal_to: 9999999 }   
 end

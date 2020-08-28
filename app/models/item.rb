8fd1a8b9a711993d6b13eafa76b6a5a5828c7293
hidefumiyasui <skyblue6000@icloud.com>
2020-08-28 18:13:56 +0900

    バリデーションの修正及びテストコードの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,17 +8,15 @@ class Item < ApplicationRecord
   belongs_to_active_hash :shipping_region
   belongs_to_active_hash :delivery_days
 
-  validates :image_url, presence: true
-  validates :name, presence: true,
-                   length: { maximum: 40 }
-  validates :explanation, presence: true,
-                          length: { maximum: 1000 }
-  validates :categorie_id, numericality: { other_than: 1 }
-  validates :condition_id, numericality: { other_than: 1 }
-  validates :shipping_charge_id, numericality: { other_than: 1 }
-  validates :shipping_region_id, numericality: { other_than: 1 }
-  validates :delivery_days_id, numericality: { other_than: 1 }
-  validates :price, presence: true,
-                    numericality: { greater_than_or_equal_to: 300 },
-                    numericality: { less_than_or_equal_to: 9_999_999 }
+  with_options presence: true do
+   validates :image_url
+   validates :name, length: { maximum: 40 }
+   validates :explanation, length: { maximum: 1000 }
+   validates :categorie_id, numericality: { other_than: 1 }
+   validates :condition_id, numericality: { other_than: 1 }
+   validates :shipping_charge_id, numericality: { other_than: 1 }
+   validates :shipping_region_id, numericality: { other_than: 1 }
+   validates :delivery_days_id, numericality: { other_than: 1 }
+   validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -44,30 +44,65 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Categorie is not a number')
       end
+      it 'categorie_idが1の場合保存できない' do
+        @item.categorie_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Categorie must be other than 1")
+      end
       it 'condition_idが空だと保存できない' do
         @item.condition_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include('Condition is not a number')
+      end
+      it 'condition_idが1の場合保存できない' do
+        @item.condition_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition must be other than 1")
       end
       it 'shipping_charge_idが空だと保存できない' do
         @item.shipping_charge_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include('Shipping charge is not a number')
       end
+      it 'shipping_charge_idが1の場合保存できない' do
+        @item.shipping_charge_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping charge must be other than 1")
+      end
       it 'shipping_region_idが空だと保存できない' do
         @item.shipping_region_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include('Shipping region is not a number')
+      end
+      it 'shipping_region_idが1の場合保存できない' do
+        @item.shipping_region_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping region must be other than 1")
       end
       it 'delivery_days_idが空だと保存できない' do
         @item.delivery_days_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include('Delivery days is not a number')
       end
+      it 'delivery_days_idが1の場合保存できない' do
+        @item.delivery_days_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery days must be other than 1")
+      end
       it 'priceが空だと保存できない' do
         @item.price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが300円より小さい場合保存できない' do
+        @item.price = "299"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it 'priceが9999999円より大きい場合保存できない' do
+        @item.price = "10000000"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
     end
   end
